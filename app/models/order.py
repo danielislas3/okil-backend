c35060420f42267671f117eb6edec87f238775ca
Daniel Orio <danielislas3@live.com>
2024-11-22 18:16:00 -0600

    Refactor Order model to improve client relationship and adjust barista relationship

--- app/models/order.py
+++ app/models/order.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, Integer, Numeric, Enum, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, ForeignKey, Numeric, Enum, DateTime, func
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
 from app.db.session import Base
 import enum
 
@@ -19,7 +18,9 @@ class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True)
-    clientId = Column(Integer, ForeignKey("clients.id"), nullable=True)
+    clientId = Column(
+        Integer, ForeignKey("clients.id"), nullable=True
+    )  # Relación con tabla 'clients'
     baristaId = Column(Integer, ForeignKey("users.id"), nullable=False)
     totalPrice = Column(Numeric(10, 2), nullable=False)
     discount = Column(Numeric(10, 2), default=0)
@@ -29,5 +30,5 @@ class Order(Base):
     createdAt = Column(DateTime, server_default=func.now())
     updatedAt = Column(DateTime, onupdate=func.now())
 
-    barista = relationship("User", back_populates="orders")
     client = relationship("Client", back_populates="orders")
+    barista = relationship("User", back_populates="orders")
