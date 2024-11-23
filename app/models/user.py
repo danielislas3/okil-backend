3cbae54c2f0ed16eb81169f2a0577939e6cc1e9a
Daniel Orio <danielislas3@live.com>
2024-11-22 18:06:34 -0600

    Add relationship between User and Order models

--- app/models/user.py
+++ app/models/user.py
@@ -1,16 +1,19 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 # Base class for SQLAlchemy models
 Base = declarative_base()
 
+
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    orders = relationship("Order", back_populates="barista")
 
     def __repr__(self):
         return f"<User(id={self.id}, name={self.name}, email={self.email})>"
